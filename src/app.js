229dad8c5cd6a31c7535a5b825211534404b61ab
Gordon Soutter <gordon.soutter@outlook.com>
2020-04-20 19:50:09 +0100

    Adds the capability to add a new todo to the list.

--- src/app.js
+++ src/app.js
@@ -3,11 +3,18 @@ import Vue from 'vue';
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Javascrit loaded');
   new Vue({
-  el: '#app',
-  data: {
-      todos: ["buy shopping", "clean bathroom", "file letters", "shave"]
+    el: '#app',
+    data: {
+        todos: ["buy shopping", "clean bathroom", "file letters", "shave"],
+        newTodo: ""
+    },
 
-  }
+    methods: {
+      addNewTodo: function(){
+        this.todos.push(this.newTodo);
+        this.newTodo = "";
+      }
+    }
 
 
   });
